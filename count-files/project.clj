8ea9f604b11ee9967c3f0c2c33c800f9c84fc09f
Olivier Lange <olange@petit-atelier.ch>
2013-07-21 01:05:34 +0200

    Ajouté documentation de l'API (générée par Marginalia)

--- count-files/project.clj
+++ count-files/project.clj
@@ -3,9 +3,17 @@
   :url "https://github.com/olange/toolbox/tree/master/count-files"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [
     [org.clojure/clojure "1.5.1"]
     [org.clojure/tools.cli "0.2.2"]
-    [me.raynes/fs "1.4.4"]
-   ]
-  :main count-files.core)
+    [me.raynes/fs "1.4.4"]]
+
+  :main count-files.core
+
+  :profiles {
+  	:dev {
+      :plugins [[lein-marginalia "0.7.1"]]
+      :aliases {"docs" ^{:doc "Generates Marginalia documentation files"} ["do" "marg" "--dir" "doc"]}
+    }
+  })
